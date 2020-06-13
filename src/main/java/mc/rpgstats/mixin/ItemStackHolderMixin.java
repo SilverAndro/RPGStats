e8d2adcf0dce3cbaeb6f396998d038d9fb55ed29
P03W <pweidman.mail@gmail.com>
2020-06-13 14:46:41 -0400

    Clean up imports

--- src/main/java/mc/rpgstats/mixin/ItemStackHolderMixin.java
+++ src/main/java/mc/rpgstats/mixin/ItemStackHolderMixin.java
@@ -1,7 +1,6 @@
 package mc.rpgstats.mixin;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;

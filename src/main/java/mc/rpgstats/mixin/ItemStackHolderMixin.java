be357488dbe2aebf5d726a6e8c6b98e004fa5d59
P03W <pweidman.mail@gmail.com>
2020-06-13 19:06:48 -0400

    Implaing does damageto all at lv 50

--- src/main/java/mc/rpgstats/mixin/ItemStackHolderMixin.java
+++ src/main/java/mc/rpgstats/mixin/ItemStackHolderMixin.java
@@ -8,13 +8,22 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackHolderMixin {
     @Shadow public abstract void setHolder(Entity holder);
 
+    @Shadow public abstract Entity getHolder();
+
     @Inject(at = @At("HEAD"), method = "inventoryTick")
     private void onTick(World world, Entity entity, int slot, boolean selected, CallbackInfo ci) {
         setHolder(entity);
+    }
+
+    @Inject(at = @At("TAIL"), method = "copy", locals = LocalCapture.CAPTURE_FAILHARD)
+    private void copyHolder(CallbackInfoReturnable<ItemStack> cir, ItemStack itemStack) {
+        itemStack.setHolder(this.getHolder());
     }
 }

33aabfe4f2e1faf3e96ad838afb9555e3513c54d
P03W <P03W@users.noreply.github.com>
2020-07-03 23:08:47 -0400

    Make using a totem of undying grant defense XP

---
+++ src/main/java/mc/rpgstats/mixin/TotemUseMixin.java
@@ -0,0 +1,29 @@
+package mc.rpgstats.mixin;
+
+import mc.rpgstats.main.RPGStats;
+import nerdhub.cardinal.components.api.component.ComponentProvider;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(LivingEntity.class)
+public class TotemUseMixin {
+    @Inject(
+        method = "tryUseTotem(Lnet/minecraft/entity/damage/DamageSource;)Z",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/server/network/ServerPlayerEntity;incrementStat(Lnet/minecraft/stat/Stat;)V",
+            shift = At.Shift.AFTER
+        ),
+        locals = LocalCapture.CAPTURE_FAILHARD
+    )
+    public void onUseTotem(DamageSource source, CallbackInfoReturnable<Boolean> cir, ItemStack itemStack, ServerPlayerEntity serverPlayerEntity) {
+        RPGStats.addXpAndLevelUpIfNeeded(RPGStats.DEFENSE_COMPONENT, ComponentProvider.fromEntity(serverPlayerEntity), 130);
+    }
+}

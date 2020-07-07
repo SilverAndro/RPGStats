818a3eaa644f3873a57d46969da99b9c91ac74db
P03W <pweidman.mail@gmail.com>
2020-07-06 21:42:15 -0400

    Everything is working other than mcf-reap
    issue tracking compatibility - https://github.com/maxvar/mcf-reap/issues/8

--- src/main/java/mc/rpgstats/mixin/MaxvarReapCompatMixin.java
+++ src/main/java/mc/rpgstats/mixin/MaxvarReapCompatMixin.java
@@ -1,12 +1,11 @@
 package mc.rpgstats.mixin;
 
-import mc.rpgstats.main.RPGStats;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.CropBlock;
+import net.minecraft.block.PlantBlock;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.state.property.IntProperty;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -14,17 +13,21 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
 import java.util.List;
 import java.util.Set;
 
-@Mixin(AbstractBlock.class)
-public class MaxvarReapCompatMixin implements IMixinConfigPlugin {
+import static net.minecraft.block.CropBlock.AGE;
+
+@SuppressWarnings("unused")
+@Mixin(CropBlock.class)
+public class MaxvarReapCompatMixin extends PlantBlock implements IMixinConfigPlugin {
+    
+    protected MaxvarReapCompatMixin(Settings settings) {
+        super(settings);
+    }
     
     @Override
     public void onLoad(String mixinPackage) {}
@@ -51,12 +54,24 @@ public class MaxvarReapCompatMixin implements IMixinConfigPlugin {
     @Override
     public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) { }
     
-    @Inject(method = "onUse", at = @At("HEAD"))
-    public void onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
-        if (!world.isClient && state.getBlock() instanceof CropBlock) {
-            if (((CropBlock)state.getBlock()).isMature(state)) {
-                RPGStats.addXpAndLevelUp(RPGStats.FARMING_COMPONENT, (ServerPlayerEntity)player, 1);
-            }
+    @SuppressWarnings({"SameReturnValue", "unused"})
+    @Shadow
+    public boolean isMature(BlockState state) {
+        return true;
+    }
+    
+    @SuppressWarnings("SameReturnValue")
+    @Shadow
+    public IntProperty getAgeProperty() {
+        return AGE;
+    }
+    
+    @SuppressWarnings("deprecation")
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (!world.isClient() & isMature(state)) {
+            
         }
+        return super.onUse(state, world, pos, player, hand, hit);
     }
 }

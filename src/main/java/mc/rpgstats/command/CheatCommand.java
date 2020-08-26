6ecf3c45adbcdc0e21df3aba9544f4a3eca68155
P03W <P03W@users.noreply.github.com>
2020-08-25 20:31:17 -0400

    Let rpgcheat set xp and level for stats

--- src/main/java/mc/rpgstats/command/CheatCommand.java
+++ src/main/java/mc/rpgstats/command/CheatCommand.java
@@ -1,37 +1,101 @@
 package mc.rpgstats.command;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import mc.rpgstats.main.RPGStats;
+import nerdhub.cardinal.components.api.component.ComponentProvider;
+import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Identifier;
 
 import java.util.Collection;
+
+import static com.mojang.brigadier.arguments.StringArgumentType.string;
 
 public class CheatCommand {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(
             // Base
             CommandManager.literal("rpgcheat")
-                // Add xp/levels
+                // Choose skill
                 .then(
-                    CommandManager.literal("add")
-                        .then(CommandManager.literal("xp"))
-                        .then(CommandManager.literal("levels"))
-                )
-                // Set xp/levels
-                .then(
-                    CommandManager.literal("set")
-                        .then(CommandManager.literal("xp"))
-                        .then(CommandManager.literal("levels"))
+                    // Select skill
+                    CommandManager.argument("skill", string())
+                        .suggests(new SkillSuggestionProvider())
+                        .then(
+                            CommandManager.argument("targets", EntityArgumentType.players())
+                                // Add xp/levels
+                                .then(
+                                    CommandManager.literal("add")
+                                        .then(
+                                            CommandManager.literal("xp")
+                                        )
+                                        .then(
+                                            CommandManager.literal("levels")
+                                        )
+                                )
+                                // Set xp/levels
+                                .then(
+                                    CommandManager.literal("set")
+                                        // Set XP
+                                        .then(
+                                            CommandManager.literal("xp")
+                                                .then(
+                                                    CommandManager.argument("amount", IntegerArgumentType.integer(0))
+                                                        .executes(
+                                                            (commandContext) -> executeSet(
+                                                                commandContext.getSource(),
+                                                                new Identifier(StringArgumentType.getString(commandContext, "skill")),
+                                                                EntityArgumentType.getPlayers(commandContext, "targets"),
+                                                                CommandType.XP,
+                                                                IntegerArgumentType.getInteger(commandContext, "amount")
+                                                            )
+                                                        )
+                                                )
+                                        )
+                                        // Set Levels
+                                        .then(
+                                            CommandManager.literal("levels")
+                                        )
+                                        .then(
+                                            CommandManager.argument("amount", IntegerArgumentType.integer(0, 50))
+                                                .executes(
+                                                    (commandContext) -> executeSet(
+                                                        commandContext.getSource(),
+                                                        new Identifier(StringArgumentType.getString(commandContext, "skill")),
+                                                        EntityArgumentType.getPlayers(commandContext, "targets"),
+                                                        CommandType.LEVELS,
+                                                        IntegerArgumentType.getInteger(commandContext, "amount")
+                                                    )
+                                                )
+                                        )
+                                )
+                        )
                 )
         );
     }
     
-    private static int executeAdd(ServerCommandSource source, Collection<ServerPlayerEntity> targets) {
-        return 0;
+    private static int executeAdd(ServerCommandSource source, Collection<ServerPlayerEntity> targets, CommandType type) {
+        return 1;
     }
     
-    private static int executeSet(ServerCommandSource source, Collection<ServerPlayerEntity> targets) {
-        return 0;
+    private static int executeSet(ServerCommandSource source, Identifier id, Collection<ServerPlayerEntity> targets, CommandType type, int amount) {
+        for (ServerPlayerEntity target : targets) {
+            if (type == CommandType.XP) {
+                RPGStats.setComponentXP(RPGStats.statFromID(id), ComponentProvider.fromEntity(target), amount);
+            }
+            if (type == CommandType.LEVELS) {
+                RPGStats.setComponentLevel(RPGStats.statFromID(id), ComponentProvider.fromEntity(target), amount);
+            }
+        }
+        return 1;
+    }
+    
+    enum CommandType {
+        LEVELS,
+        XP
     }
 }
---
+++ src/main/java/mc/rpgstats/command/SkillSuggestionProvider.java
@@ -0,0 +1,22 @@
+package mc.rpgstats.command;
+
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import mc.rpgstats.main.RPGStats;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.util.Identifier;
+
+import java.util.concurrent.CompletableFuture;
+
+public class SkillSuggestionProvider implements SuggestionProvider<ServerCommandSource> {
+    @Override
+    public CompletableFuture<Suggestions> getSuggestions(CommandContext<ServerCommandSource> context, SuggestionsBuilder builder) throws CommandSyntaxException {
+        for (Identifier skillID : RPGStats.idToComponentIndexMap.keySet()) {
+            builder.suggest('"' + skillID.toString() + '"');
+        }
+        return builder.buildFuture();
+    }
+}

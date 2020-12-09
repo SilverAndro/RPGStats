40cb2e5082fb90078a095591d40523e480272bb1
P03W <P03W@users.noreply.github.com>
2020-12-08 20:56:40 -0500

    Make nix shoot arrows that cant be picked up

---
+++ src/main/java/mc/rpgstats/mixin/BowCanUseMixin.java
@@ -0,0 +1,25 @@
+package mc.rpgstats.mixin;
+
+import mc.rpgstats.main.RPGStats;
+import mc.rpgstats.main.StatComponents;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerEntity.class)
+public class BowCanUseMixin {
+    @Inject(method = "getArrowType", at = @At("HEAD"), cancellable = true)
+    public void modifyGetArrow(ItemStack stack, CallbackInfoReturnable<ItemStack> cir) {
+        //noinspection ConstantConditions
+        if ((Object)this instanceof ServerPlayerEntity) {
+            if (RPGStats.getComponentLevel(StatComponents.RANGED_COMPONENT, (ServerPlayerEntity)(Object)this) >= 50) {
+                cir.setReturnValue(new ItemStack(Items.ARROW));
+            }
+        }
+    }
+}

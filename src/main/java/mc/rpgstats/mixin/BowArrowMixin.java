b6f02963b85f5878a61c6761cb9e9c3ab05a6e22
P03W <pweidman.mail@gmail.com>
2020-12-08 20:56:40 -0500

    Make nix shoot arrows that cant be picked up

--- src/main/java/mc/rpgstats/mixin/BowArrowMixin.java
+++ src/main/java/mc/rpgstats/mixin/BowArrowMixin.java
@@ -2,23 +2,33 @@ package mc.rpgstats.mixin;
 
 import mc.rpgstats.main.RPGStats;
 import mc.rpgstats.main.StatComponents;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.BowItem;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(PlayerEntity.class)
+@Mixin(BowItem.class)
 public class BowArrowMixin {
-    @Inject(at = @At(value = "TAIL"), method = "getArrowType", cancellable = true)
-    public void noArrowRequired(ItemStack stack, CallbackInfoReturnable<ItemStack> cir) {
-        if (stack.getHolder() != null && stack.getHolder() instanceof PlayerEntity) {
-            if (cir.getReturnValue().getItem() == Items.AIR && RPGStats.getComponentLevel(StatComponents.RANGED_COMPONENT, (ServerPlayerEntity)stack.getHolder()) >= 50) {
-                cir.setReturnValue(new ItemStack(Items.ARROW));
-            }
+    private ServerPlayerEntity itemUser = null;
+    
+    @Inject(at = @At(value = "HEAD"), method = "onStoppedUsing", cancellable = true)
+    public void capturePlayerUsingBow(ItemStack stack, World world, LivingEntity user, int remainingUseTicks, CallbackInfo ci) {
+        if (user instanceof ServerPlayerEntity) {
+            itemUser = (ServerPlayerEntity)user;
         }
+    }
+    
+    @ModifyVariable(method = "onStoppedUsing", at = @At("INVOKE_ASSIGN"), name = "bl")
+    public boolean createArrowIfHasNix(boolean bl) {
+        if (RPGStats.getComponentLevel(StatComponents.RANGED_COMPONENT, itemUser) >= 50) {
+            return true;
+        }
+        return bl;
     }
 }

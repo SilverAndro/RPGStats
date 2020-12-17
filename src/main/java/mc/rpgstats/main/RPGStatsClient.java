c9b10bb85b61a081129f6e36cf4e1c40990b0168
P03W <pweidman.mail@gmail.com>
2020-12-16 19:40:05 -0500

    Update to networking v1

--- src/main/java/mc/rpgstats/main/RPGStatsClient.java
+++ src/main/java/mc/rpgstats/main/RPGStatsClient.java
@@ -1,7 +1,7 @@
 package mc.rpgstats.main;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Pair;
 
@@ -12,21 +12,19 @@ public class RPGStatsClient implements ClientModInitializer {
     
     @Override
     public void onInitializeClient() {
-        ClientSidePacketRegistry.INSTANCE.register(RPGStats.SYNC_STATS_PACKET_ID,
-            (packetContext, attachedData) -> {
-                // Read data
-                
-                // Get the amount of stats to read
-                int count = attachedData.readInt();
-                
-                // Read each stat in turn
-                for (int i = 0; i < count; i++) {
-                    Identifier statIdent = attachedData.readIdentifier();
-                    int level = attachedData.readInt();
-                    int xp = attachedData.readInt();
-                    currentStats.put(statIdent, new Pair<>(level, xp));
-                }
+        ClientPlayNetworking.registerGlobalReceiver(RPGStats.SYNC_STATS_PACKET_ID, (client, handler, byteBuf, packetSender) -> {
+            // Read data
+    
+            // Get the amount of stats to read
+            int count = byteBuf.readInt();
+    
+            // Read each stat in turn
+            for (int i = 0; i < count; i++) {
+                Identifier statIdent = byteBuf.readIdentifier();
+                int level = byteBuf.readInt();
+                int xp = byteBuf.readInt();
+                currentStats.put(statIdent, new Pair<>(level, xp));
             }
-        );
+        });
     }
 }

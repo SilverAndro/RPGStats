ba703412ba441ae9b8e0c815e97f81cbbedd6c31
P03W <pweidman.mail@gmail.com>
2021-01-13 21:07:01 -0500

    Add hardcore mode (stats lost on death)
    closes #20

--- src/main/java/mc/rpgstats/main/StatComponents.java
+++ src/main/java/mc/rpgstats/main/StatComponents.java
@@ -34,13 +34,23 @@ public class StatComponents implements EntityComponentInitializer {
         MINING_COMPONENT = registerSkill(new Identifier("rpgstats:mining"), MiningComponent.class, registry);
         FISHING_COMPONENT = registerSkill(new Identifier("rpgstats:fishing"), FishingComponent.class, registry);
         
-        registry.beginRegistration(PlayerEntity.class, MELEE_COMPONENT).impl(MeleeComponent.class).respawnStrategy(RespawnCopyStrategy.ALWAYS_COPY).end(MeleeComponent::new);
-        registry.beginRegistration(PlayerEntity.class, RANGED_COMPONENT).impl(RangedComponent.class).respawnStrategy(RespawnCopyStrategy.ALWAYS_COPY).end(RangedComponent::new);
-        registry.beginRegistration(PlayerEntity.class, DEFENSE_COMPONENT).impl(DefenseComponent.class).respawnStrategy(RespawnCopyStrategy.ALWAYS_COPY).end(DefenseComponent::new);
-        registry.beginRegistration(PlayerEntity.class, FARMING_COMPONENT).impl(FarmingComponent.class).respawnStrategy(RespawnCopyStrategy.ALWAYS_COPY).end(FarmingComponent::new);
-        registry.beginRegistration(PlayerEntity.class, MAGIC_COMPONENT).impl(MagicComponent.class).respawnStrategy(RespawnCopyStrategy.ALWAYS_COPY).end(MagicComponent::new);
-        registry.beginRegistration(PlayerEntity.class, MINING_COMPONENT).impl(MiningComponent.class).respawnStrategy(RespawnCopyStrategy.ALWAYS_COPY).end(MiningComponent::new);
-        registry.beginRegistration(PlayerEntity.class, FISHING_COMPONENT).impl(FishingComponent.class).respawnStrategy(RespawnCopyStrategy.ALWAYS_COPY).end(FishingComponent::new);
+        if (RPGStats.getConfig().hardcoreMode) {
+            registry.beginRegistration(PlayerEntity.class, MELEE_COMPONENT).impl(MeleeComponent.class).end(MeleeComponent::new);
+            registry.beginRegistration(PlayerEntity.class, RANGED_COMPONENT).impl(RangedComponent.class).end(RangedComponent::new);
+            registry.beginRegistration(PlayerEntity.class, DEFENSE_COMPONENT).impl(DefenseComponent.class).end(DefenseComponent::new);
+            registry.beginRegistration(PlayerEntity.class, FARMING_COMPONENT).impl(FarmingComponent.class).end(FarmingComponent::new);
+            registry.beginRegistration(PlayerEntity.class, MAGIC_COMPONENT).impl(MagicComponent.class).end(MagicComponent::new);
+            registry.beginRegistration(PlayerEntity.class, MINING_COMPONENT).impl(MiningComponent.class).end(MiningComponent::new);
+            registry.beginRegistration(PlayerEntity.class, FISHING_COMPONENT).impl(FishingComponent.class).end(FishingComponent::new);
+        } else {
+            registry.beginRegistration(PlayerEntity.class, MELEE_COMPONENT).impl(MeleeComponent.class).respawnStrategy(RespawnCopyStrategy.ALWAYS_COPY).end(MeleeComponent::new);
+            registry.beginRegistration(PlayerEntity.class, RANGED_COMPONENT).impl(RangedComponent.class).respawnStrategy(RespawnCopyStrategy.ALWAYS_COPY).end(RangedComponent::new);
+            registry.beginRegistration(PlayerEntity.class, DEFENSE_COMPONENT).impl(DefenseComponent.class).respawnStrategy(RespawnCopyStrategy.ALWAYS_COPY).end(DefenseComponent::new);
+            registry.beginRegistration(PlayerEntity.class, FARMING_COMPONENT).impl(FarmingComponent.class).respawnStrategy(RespawnCopyStrategy.ALWAYS_COPY).end(FarmingComponent::new);
+            registry.beginRegistration(PlayerEntity.class, MAGIC_COMPONENT).impl(MagicComponent.class).respawnStrategy(RespawnCopyStrategy.ALWAYS_COPY).end(MagicComponent::new);
+            registry.beginRegistration(PlayerEntity.class, MINING_COMPONENT).impl(MiningComponent.class).respawnStrategy(RespawnCopyStrategy.ALWAYS_COPY).end(MiningComponent::new);
+            registry.beginRegistration(PlayerEntity.class, FISHING_COMPONENT).impl(FishingComponent.class).respawnStrategy(RespawnCopyStrategy.ALWAYS_COPY).end(FishingComponent::new);
+        }
     }
     
     public static <T extends IStatComponent> ComponentKey<T> registerSkill(Identifier componentID, Class<T> componentClass, EntityComponentFactoryRegistry registry) {

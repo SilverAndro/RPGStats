b36d50a0a3d25de631d15986950e9c77c49f6f05
P03W <P03W@users.noreply.github.com>
2021-05-26 18:42:55 -0400

    Update fabric.mod.json, stop using mixin plugin for compat

--- src/main/java/mc/rpgstats/mixin/compat/harvest_scythes/GrantFarmingOnUseScytheMixin.java
+++ src/main/java/mc/rpgstats/mixin/compat/harvest_scythes/GrantFarmingOnUseScytheMixin.java
@@ -13,12 +13,14 @@ import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import wraith.harvest_scythes.ScytheTool;
 
+@Pseudo
 @Mixin(ScytheTool.class)
 public class GrantFarmingOnUseScytheMixin {
     @Inject(method = "harvest",

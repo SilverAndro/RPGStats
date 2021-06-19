7fc42ef0f8a10decd7e3fc0e2b36b31167a611c7
P03W <P03W@users.noreply.github.com>
2021-06-19 16:43:01 -0400

    Fix gui from command

--- src/main/java/mc/rpgstats/client/screen/RPGStatDisplayGUI.java
+++ src/main/java/mc/rpgstats/client/screen/RPGStatDisplayGUI.java
@@ -18,6 +18,7 @@ public class RPGStatDisplayGUI extends LightweightGuiDescription {
     ArrayList<Identifier> data = new ArrayList<>();
     
     public RPGStatDisplayGUI() {
+        super();
         data.addAll(RPGStatsClient.currentStats.keySet());
         
         WGridPanel root = new WGridPanel();
@@ -25,10 +26,11 @@ public class RPGStatDisplayGUI extends LightweightGuiDescription {
         root.setSize(240, 180);
         
         WLabel guiTitle = new WLabel("  RPGStats");
-        root.add(guiTitle, 5, 0);
+        root.add(guiTitle, 5, 1);
     
         BiConsumer<Identifier, StatEntry> configurator = (Identifier identifier, StatEntry entry) -> {
             int level = RPGStatsClient.currentStats.get(identifier).getLeft();
+            System.out.println(identifier);
             int xp = RPGStatsClient.currentStats.get(identifier).getRight();
             
             String name = RPGStatsClient.nameMap.get(identifier);
@@ -42,7 +44,7 @@ public class RPGStatDisplayGUI extends LightweightGuiDescription {
     
         WListPanel<Identifier, StatEntry> list = new WListPanel<>(data, StatEntry::new, configurator);
         list.setListItemHeight(18);
-        root.add(list, 1, 1, 11, 8);
+        root.add(list, 1, 2, 11, 7);
         
         root.validate(this);
     }

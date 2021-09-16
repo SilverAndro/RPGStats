844a85eea828bf68a145513e4cfb0d6b5625f0d7
P03W <P03W@users.noreply.github.com>
2021-09-16 16:06:03 -0400

    Tweak shield xp, add damage source blacklist (closes #76)

--- src/main/java/mc/rpgstats/mixin/ApplyDamageMixin.java
+++ src/main/java/mc/rpgstats/mixin/ApplyDamageMixin.java
@@ -2,6 +2,7 @@ package mc.rpgstats.mixin;
 
 import mc.rpgstats.main.RPGStats;
 import mc.rpgstats.main.CustomComponents;
+import mc.rpgstats.main.RPGStatsConfig;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -22,7 +23,7 @@ public class ApplyDamageMixin {
     @Inject(method = "applyDamage", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/entity/player/PlayerEntity;applyEnchantmentsToDamage(Lnet/minecraft/entity/damage/DamageSource;F)F", shift = At.Shift.AFTER))
     public void grantXpFromDamageAbsorbedThroughArmorOrEnchants(DamageSource source, float amount, CallbackInfo ci) {
         //noinspection ConstantConditions
-        if ((Object)this instanceof ServerPlayerEntity) {
+        if ((Object)this instanceof ServerPlayerEntity && sourceCanGrantXp(source)) {
             float blockedDamage = originalDamage - amount;
             if (blockedDamage <= 3) {
                 return;
@@ -32,5 +33,19 @@ public class ApplyDamageMixin {
                 (ServerPlayerEntity)(Object)this,
                 Math.min((int)Math.floor(Math.log(Math.pow(blockedDamage, 5.0f))), 4));
         }
+    }
+    
+    public boolean sourceCanGrantXp(DamageSource source) {
+        RPGStatsConfig.DamageSourceBlacklist blacklist = RPGStats.getConfig().damageBlacklist;
+        if (source == DamageSource.LIGHTNING_BOLT) return blacklist.lightning;
+        if (source == DamageSource.LAVA) return blacklist.lava;
+        if (source == DamageSource.HOT_FLOOR) return blacklist.hotFloor;
+        if (source == DamageSource.CACTUS) return blacklist.cactus;
+        if (source == DamageSource.ANVIL) return blacklist.anvil;
+        if (source == DamageSource.FALLING_BLOCK) return blacklist.fallingBlock;
+        if (source == DamageSource.DRYOUT) return blacklist.dryOut;
+        if (source == DamageSource.SWEET_BERRY_BUSH) return blacklist.berryBush;
+        if (source == DamageSource.STALAGMITE) return blacklist.stalactite;
+        return true;
     }
 }

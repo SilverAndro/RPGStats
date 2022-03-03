89d7c06472389de621fff7743fdfe6f123bd6f4a
P03W <P03W@users.noreply.github.com>
2022-03-03 16:21:02 -0500

    1.18.2 proper

--- src/main/java/mc/rpgstats/mixin/compat/artifality/GrantFarmingOnStaffHarvest.java
+++ src/main/java/mc/rpgstats/mixin/compat/artifality/GrantFarmingOnStaffHarvest.java
@@ -1,8 +1,8 @@
 package mc.rpgstats.mixin.compat.artifality;
 
-import artifality.item.HarvestStaffItem;
 import mc.rpgstats.main.CustomComponents;
 import mc.rpgstats.main.RPGStats;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
@@ -13,8 +13,12 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Pseudo
-@Mixin(HarvestStaffItem.class)
-public class GrantFarmingOnStaffHarvest {
+@Mixin(targets = "artifality.item.HarvestStaffItem")
+public abstract class GrantFarmingOnStaffHarvest extends Item {
+    public GrantFarmingOnStaffHarvest(Settings settings) {
+        super(settings);
+    }
+    
     @Inject(method = "useOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)Z"))
     private void rpgstats$giveFarmingXpOnStaffHarvest(ItemUsageContext crop, CallbackInfoReturnable<ActionResult> cir){
         if (crop.getPlayer() instanceof ServerPlayerEntity player) {

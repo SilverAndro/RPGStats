3ae47363a985c4a853dde0f19d70e67394e8557c
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2022-09-03 20:52:59 +0300

    optimize imports

--- src/main/java/mc/rpgstats/mixin/BowAccuracyMixin.java
+++ src/main/java/mc/rpgstats/mixin/BowAccuracyMixin.java
@@ -1,22 +1,13 @@
 package mc.rpgstats.mixin;
 
-import mc.rpgstats.main.RPGStats;
 import mc.rpgstats.main.CustomComponents;
+import mc.rpgstats.main.RPGStats;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.projectile.PersistentProjectileEntity;
-import net.minecraft.item.ArrowItem;
 import net.minecraft.item.BowItem;
-import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(BowItem.class)
 class BowAccuracyMixin {

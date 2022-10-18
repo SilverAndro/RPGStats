5c8a729e27f87b37f4b722d5f7a9544bdf3417da
SilverAndro <52360088+SilverAndro@users.noreply.github.com>
2022-10-17 21:32:26 -0400

    Use yet-to be merged QKL PR for codec serialization
    Yes this wont compile shhhh, just wait

--- src/main/kotlin/io/github/silverandro/rpgstats/datadrive/xp/XpData.kt
+++ src/main/kotlin/io/github/silverandro/rpgstats/datadrive/xp/XpData.kt
@@ -2,22 +2,22 @@ package io.github.silverandro.rpgstats.datadrive.xp
 
 import com.mojang.datafixers.util.Either
 import com.mojang.serialization.Codec
-import com.mojang.serialization.codecs.RecordCodecBuilder
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
+import org.quiltmc.qkl.wrapper.minecraft.serialization.CodecFactory
 import org.quiltmc.qsl.registry.attachment.api.RegistryEntryAttachment
 
 object XpData {
-    data class XpEntry(val id: Identifier, val amount: Int)
+    @Serializable
+    data class XpEntry(val id: @Contextual Identifier, val amount: Int)
 
-    private val recordCodec: Codec<XpEntry> = RecordCodecBuilder.create { instance ->
-        instance.group(
-            Identifier.CODEC.fieldOf("id").forGetter { it.id },
-            Codec.INT.fieldOf("amount").forGetter { it.amount }
-        ).apply(instance) { id: Identifier, amount: Int ->
-            XpEntry(id, amount)
+    private val xpEntryCodec: Codec<XpEntry> = CodecFactory {
+        codecs {
+            named(Identifier.CODEC, "Identifier")
         }
-    }
+    }.create()
 
     val BLOCK_XP = makeREA(Registry.BLOCK, "block")
     val ENTITY_XP = makeREA(Registry.ENTITY_TYPE, "entity")
@@ -28,7 +28,7 @@ object XpData {
                 registry,
                 Identifier("rpgstats", "${name}_xp"),
                 Either::class.java as Class<Either<XpEntry, MutableList<XpEntry>>>,
-                Codec.either(recordCodec, Codec.list(recordCodec))
+                Codec.either(xpEntryCodec, Codec.list(xpEntryCodec))
         ).build() as RegistryEntryAttachment<T, Either<XpEntry, MutableList<XpEntry>>>
     }
 

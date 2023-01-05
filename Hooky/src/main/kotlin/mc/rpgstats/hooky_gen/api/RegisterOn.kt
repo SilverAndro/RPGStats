3d912edeb610a1e7e7d5a80b35b473248603b22a
SilverAndro <52360088+silverandro@users.noreply.github.com>
2023-01-04 20:03:55 -0500

    We do a little licensing

--- Hooky/src/main/kotlin/mc/rpgstats/hooky_gen/api/RegisterOn.kt
+++ Hooky/src/main/kotlin/mc/rpgstats/hooky_gen/api/RegisterOn.kt
@@ -1,5 +1,11 @@
+/*
+ *   This Source Code Form is subject to the terms of the Mozilla Public
+ *   License, v. 2.0. If a copy of the MPL was not distributed with this
+ *   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package mc.rpgstats.hooky_gen.api
 
 @Retention(AnnotationRetention.SOURCE)
 @Target(AnnotationTarget.FUNCTION)
-annotation class RegisterOn(val event: String)
+annotation class RegisterOn(@Suppress("unused") val event: String)

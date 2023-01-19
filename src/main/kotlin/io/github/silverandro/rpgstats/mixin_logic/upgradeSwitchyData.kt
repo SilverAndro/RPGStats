02edd07a4b3693616e0a711b6f95d5aa01b4067c
SilverAndro <52360088+silverandro@users.noreply.github.com>
2023-01-18 21:44:34 -0500

    debug be gone

--- src/main/kotlin/io/github/silverandro/rpgstats/mixin_logic/upgradeSwitchyData.kt
+++ src/main/kotlin/io/github/silverandro/rpgstats/mixin_logic/upgradeSwitchyData.kt
@@ -4,18 +4,13 @@ import net.minecraft.nbt.NbtCompound
 
 // input should be "switchy:presets"/"list"
 fun upgradeSwitchyData(profileList: NbtCompound) {
-    println("Upgrading switchy data for rpgstats...")
-
     data class UpgradeEntry(var stats: NbtCompound?, var internal: NbtCompound?) { constructor() : this(null, null) }
     val toUpgrade = mutableMapOf<String, UpgradeEntry>()
 
     // Extract
-    println("Extracting old data...")
     profileList.keys.forEach { profileName ->
-        println("Profile: $profileName")
         val profileNbt = profileList.getCompound(profileName)
         profileNbt.keys.forEach { moduleName ->
-            println("Module: $moduleName")
             when (moduleName) {
                 "rpgstats:switchy_compat" -> toUpgrade.computeIfAbsent(profileName) { UpgradeEntry() }.stats = profileNbt.getCompound(moduleName)
                 "rpgstats:switchy_compat_internal" -> toUpgrade.computeIfAbsent(profileName) { UpgradeEntry() }.internal = profileNbt.getCompound(moduleName)
@@ -24,12 +19,9 @@ fun upgradeSwitchyData(profileList: NbtCompound) {
         }
     }
 
-    println(toUpgrade)
-    println("Restoring data...")
     // Restore
     toUpgrade.forEach {  (profileName, entry) ->
         val (stats, internal) = entry
-        println("Profile: $profileName ${stats != null} ${internal != null}")
         profileList.getCompound(profileName).put("rpgstats:stats", NbtCompound().apply {
             if (stats != null) {
                 put("rpgstats:stats", stats)
